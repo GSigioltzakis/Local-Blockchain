5f2d5468d010e6118980b22fa2059a2d383e3a83
GEORGIOS <csd5504@csd.uoc.gr>
2026-03-08 01:13:12 +0200

    function added

--- include/block.h
+++ include/block.h
@@ -2,23 +2,27 @@
 #define BLOCK_H
 
 #include <string>
+#include <iostream>
 #include <vector>
 #include <ctime>
+#include "trans.h"
 
 class Block {
 public:
     int index;
     long long timestamp;
-    std::string data; //must change to a vector of transactions later, for now we just use a string to represent the data in the block
+    std::vector<Transaction> transactions; //transactions included in this block (Merkle root will be used for hashing)
     std::string prevHash;
     std::string hash;
     int nonce; //proof of work
 
-    Block(int idx, const std::string& d, const std::string& prev);
+    Block(int idx, const std::vector<Transaction>& txs, const std::string& prev);
     
     std::string calculateHash() const;
     
     void mineBlock(int difficulty); 
+    // print merkle tree (one-time visual). Doesn't change hashing behavior.
+    void printMerkleTree() const;
 };
 
 #endif
